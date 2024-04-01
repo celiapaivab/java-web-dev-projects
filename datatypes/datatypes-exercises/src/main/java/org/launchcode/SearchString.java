176382fd36a57f02ef370279c607b2d0bafa7a7c
celiapaivab <celiadepaivabruno@gmail.com>
2024-04-01 18:53:19 -0500

    fixing exercises data types

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/SearchString.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/SearchString.java
@@ -8,20 +8,20 @@ public class SearchString {
                 " having nothing to do: once or twice she had peeped into the book her sister was reading," +
                 " but it had no pictures or conversations in it, ‘and what is the use of a book,’ thought " +
                 "Alice ‘without pictures or conversation?’";
-        alice = alice.toLowerCase();
+        String newAlice = alice.toLowerCase();
 
         Scanner input = new Scanner(System.in);
         System.out.println("Text a term to search for: ");
         String term = input.nextLine();
 
         term = term.toLowerCase();
-        boolean check = alice.contains(term);
+        boolean check = newAlice.contains(term);
         System.out.println(check);
 
-        int index = alice.indexOf(term);
+        int index = newAlice.indexOf(term);
         int length = term.length();
         System.out.println("Your search term first appears at index " + index + ". Your term is " + length + " characters long.");
-        String modifiedAlice = alice.replace(term, "(YOUR TERM WAS HERE)");
+        String modifiedAlice = newAlice.replace(term, "(YOUR TERM WAS HERE)");
         System.out.println(modifiedAlice);
 
     }
