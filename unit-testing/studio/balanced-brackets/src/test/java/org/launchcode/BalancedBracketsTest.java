1bed1bc4c21d8ad40ddf97cef66a2900e3d51874
celiapaivab <celiadepaivabruno@gmail.com>
2024-04-15 11:24:27 -0500

    Studio Unit Testing done

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,95 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    //Test brackets in the correct order, "[]"
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    //Test brackets in front of other characters, "[]LaunchCode"
+    @Test
+    public void bracketsFollowedByCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    //Test brackets around other characters, "[LaunchCode]"
+    @Test
+    public void bracketsAroundCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    //Test brackets within other characters, "Launch[Code]"
+    @Test
+    public void bracketsWithinCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    //Test multiples consecutive pairs of brackets, "[][][]"
+    @Test
+    public void multipleConsecutiveBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][]"));
+    }
+
+    //Test multiples pairs of brackets within other brackets, "[[[]]]"
+    @Test
+    public void multipleNestedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
+
+    //Test single opening bracket, "["
+    @Test
+    public void singleOpeningBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    //Test single closing bracket, "]"
+    @Test
+    public void singleClosingBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    //Test reversed brackets, "]["
+    @Test
+    public void reversedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    //Test single opening bracket with other characters, "[LaunchCode"
+    @Test
+    public void singleOpeningBracketWithCharacters() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    //Test single closing bracket with other characters, "LaunchCode]"
+    @Test
+    public void singleClosingBracketWithCharacters() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]"));
+    }
+
+    //Test unbalanced bracket within other characters, "]Launch[Code"
+    @Test
+    public void unbalancedBracketsWithinCharacters() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Launch[Code"));
+    }
+
+    //Test multiple unbalanced brackets, "]]][[["
+    @Test
+    public void multipleUnbalancedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]][[["));
+    }
+
+    //Test string of characters without brackets, "LaunchCode"
+    @Test
+    public void charactersWithoutBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+
+    //Test empty string, ""
+    @Test
+    public void emptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
 }
